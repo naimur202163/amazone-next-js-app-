573c9b26937afe85ee541b412052d342232c8644
unknown <you@example.com>
2022-01-10 16:05:13 +0600

    Header Nav Link added

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import NextLink from 'next/link';
+
 import useStyles from '../utils/style';
 export default function Layout({ children }) {
   const classes = useStyles();
@@ -12,7 +20,20 @@ export default function Layout({ children }) {
       {/* Header */}
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Amazona</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" style={{ color: '#fff' }} passherf>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" style={{ color: '#fff' }} passherf>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
