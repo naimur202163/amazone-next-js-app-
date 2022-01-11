779187c76623ab529113b65342fdcd6a6ccb40c8
unknown <you@example.com>
2022-01-11 16:54:19 +0600

    hhh

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -45,7 +45,9 @@ const ProdcutScreen = () => {
         <Grid item md={3} xs={12}>
           <List>
             <ListItem>
-              <Typography component="h1">Product:{product.name}</Typography>
+              <Typography component="h1" variant="h1">
+                {product.name}
+              </Typography>
             </ListItem>
             <ListItem>
               <Typography>Category:{product.category}</Typography>
